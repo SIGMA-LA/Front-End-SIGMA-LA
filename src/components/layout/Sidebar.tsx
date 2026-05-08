d7ae828836ffba03c77085b952d67c94f8d17658
carlex <ricardogugliermino@gmail.com>
2026-05-08 01:58:38 -0300

    ocultar reportes en ADMIN

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -62,7 +62,7 @@ const menuItemsAdmin: MenuItem[] = [
   { path: '/admin/clientes', label: 'Clientes', icon: UserStar },
   { path: '/admin/obras', label: 'Obras', icon: Building2 },
   { path: '/admin/localidades', label: 'Localidades', icon: MapPin },
-  { path: '/admin/reportes', label: 'Reportes', icon: BarChart2 },
+  // { path: '/admin/reportes', label: 'Reportes', icon: BarChart2 },
   { path: '/admin/configuraciones', label: 'Configuraciones', icon: Settings },
 ]
 
